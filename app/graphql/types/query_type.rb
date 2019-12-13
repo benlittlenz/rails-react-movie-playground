29655711e999a17b89c27064933511241c05bb70
nzbenji <benlittle53@gmail.com>
2019-12-13 19:34:18 +1300

    added post and user graphql types

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :items,
+    [Types::ItemType],
+    null: false,
+    description: "Returns a list of items in the martian library"
+
+    def items
+      Item.all
+    end
+  end
+end
